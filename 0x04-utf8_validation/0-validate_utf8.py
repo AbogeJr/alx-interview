16317daf88dc4db0a8956bf1880dbda15c306dad
AbogeJr <junioraboge@gmail.com>
2023-08-27 09:32:15 +0300

    pycodestyle fixes

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,13 +3,14 @@
 UTF-8 validation
 """
 
+
 def validUTF8(data):
     """
     method that determines if a given data set
     represents a valid UTF-8 encoding
     """
     remaining_bytes = 0
-    
+
     for byte in data:
         if remaining_bytes > 0:
             if (byte >> 6) != 0b10:
@@ -17,15 +18,14 @@ def validUTF8(data):
             remaining_bytes -= 1
         else:
             if byte >> 7 == 0:
-                remaining_bytes = 0 
+                remaining_bytes = 0
             elif byte >> 5 == 0b110:
-                remaining_bytes = 1 
+                remaining_bytes = 1
             elif byte >> 4 == 0b1110:
-                remaining_bytes = 2 
+                remaining_bytes = 2
             elif byte >> 3 == 0b11110:
-                remaining_bytes = 3 
+                remaining_bytes = 3
             else:
                 return False
-        
-    return remaining_bytes == 0
 
+    return remaining_bytes == 0
